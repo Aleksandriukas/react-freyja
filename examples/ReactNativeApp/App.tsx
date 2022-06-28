2b4fa182bb8f15aa6c690e9f037e1be546415338
Aleksandras Bogdanovas <aleksandras.bogdanovas21@gmail.com>
2022-06-28 13:05:41 +0300

    Initialized pnpm

---
+++ examples/ReactNativeApp/App.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import {View} from 'react-native';
+import {Button} from '@react-freyja/core';
+
+export const App = () => {
+  return (
+    <View>
+      <Button />
+    </View>
+  );
+};
