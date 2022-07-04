91187689bd55854aeb3995f938482d9f9390ccf5
Aleksandras Sukelovic <alex.shukel@gmail.com>
2022-07-04 16:46:05 +0300

    Removed comments

--- packages/core/src/Button.tsx
+++ packages/core/src/Button.tsx
@@ -1,7 +1,6 @@
 import React from "react";
 import { Text, View } from "react-native";
 
-// FIXME Error: Unable to resolve module react from C:\Git\react-freyja\packages\core\lib\module\Button.js:
 export const Button = () => {
     return (
         <View>
