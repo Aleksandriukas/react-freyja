8589d97307cc39bcdf860e8b6cb8bbd36a82837b
Aleksandras Sukelovic <alex.shukel@gmail.com>
2022-07-04 09:47:21 +0300

    Configured storybook

---
+++ docs/storybook/storybook/rn-addons.js
@@ -0,0 +1,4 @@
+// import '@storybook/addon-ondevice-actions/manager';
+// import '@storybook/addon-ondevice-knobs/manager';
+import '@storybook/addon-ondevice-actions';
+import '@storybook/addon-ondevice-knobs';
