aca7f77a0ba61346267058d39c385209c15a739e
Aleksandras Sukelovic <alex.shukel@gmail.com>
2022-07-07 07:48:30 +0300

    Added eslint configuration

--- packages/core/src/Button.tsx
+++ packages/core/src/Button.tsx
@@ -1,18 +1,17 @@
-import React, { useContext, createContext } from "react";
-import { View, Text } from "react-native";
-import { useThemeContext } from "./useThemeContext";
+import React from 'react';
+import { View, Text } from 'react-native';
+import { useThemeContext } from './useThemeContext';
 
 export type ButtonProps = {
     //
 };
 
-export const Button: React.FC<ButtonProps> = (props) => {
-    const theme = useThemeContext()
+export const Button: React.FC<ButtonProps> = () => {
+    const theme = useThemeContext();
 
     return (
         <View style={theme.components.Button.style}>
             <Text>hello</Text>
-            </View>
+        </View>
     );
 };
-
--- packages/core/src/ThemeContextProvider.tsx
+++ packages/core/src/ThemeContextProvider.tsx
@@ -1,6 +1,6 @@
-import React, { PropsWithChildren } from "react"
-import { defaultTheme, ThemeContext } from "./ThemeContext"
+import React, { PropsWithChildren } from 'react';
+import { defaultTheme, ThemeContext } from './ThemeContext';
 
-export const ThemeContextProvider = ({children}: PropsWithChildren<{}>) => {
-    return <ThemeContext.Provider value={defaultTheme}>{children}</ThemeContext.Provider>
-}
+export const ThemeContextProvider = ({ children }: PropsWithChildren<{}>) => {
+    return <ThemeContext.Provider value={defaultTheme}>{children}</ThemeContext.Provider>;
+};
