443f6b40b3fcb1e49428eff9be817eaffb120bca
Aleksandras Sukelovic <alex.shukel@gmail.com>
2022-07-20 14:50:23 +0300

    Fixed storybook build

--- docs/storybook/storybook/stories/Button/Button.stories.js
+++ docs/storybook/storybook/stories/Button/Button.stories.js
@@ -1,5 +1,11 @@
 import {storiesOf} from '@storybook/react-native';
 import React from 'react';
 import {Button} from '@react-freyja/core';
+import {ThemeContextProvider} from '@react-freyja/theme';
+import {materialTheme} from '@react-freyja/material';
 
-storiesOf('Button', module).add('with text', () => <Button />);
+storiesOf('Button', module)
+  .addDecorator(story => (
+    <ThemeContextProvider theme={materialTheme}>{story()}</ThemeContextProvider>
+  ))
+  .add('with text', () => <Button />);
--- packages/material/src/index.ts
+++ packages/material/src/index.ts
@@ -1,1 +1,41 @@
-export * from "./materialTheme";
+import { createTheme } from "@react-freyja/theme";
+
+export const materialTheme = createTheme({
+    definitions: {
+        palette: {
+            primary: "blue",
+            secondary: "green",
+            error: "red",
+        },
+    },
+    variables: (definitions) => ({
+        primaryColor: {
+            $color: definitions.palette.primary,
+        },
+        secondaryColor: {
+            $color: definitions.palette.secondary,
+        },
+    }),
+    tokens: () => ({
+        buttonText: {
+            color: (variables) => variables.$color as string,
+        },
+        buttonOutlined: {
+            color: (variables) => variables.$color as string,
+            borderStyle: "solid",
+            borderColor: "black",
+            borderWidth: 2,
+        },
+    }),
+    components: (tokens) => ({
+        Button: {
+            tokens: [tokens.buttonText],
+            propsToTokensMap: {
+                variant: {
+                    text: tokens.buttonText,
+                    outlined: tokens.buttonOutlined,
+                },
+            },
+        },
+    }),
+});
