c2fadfa50ade05c3bcdd82779a255b5a0b23208b
Aleksandras Sukelovic <alex.shukel@gmail.com>
2022-08-21 17:05:02 +0300

    Created RNStyleEngine

--- packages/style-engine/src/index.ts
+++ packages/style-engine/src/index.ts
@@ -1,1 +1,35 @@
-export {};
+import type {
+    ExecutedTheme,
+    ExecutedThemeComponents,
+    Modifiers,
+} from "@react-freyja/types";
+import type { StyleSheet } from "react-native";
+
+export interface StyleEngineResult<TCompiledStyles> {
+    get: (component: string, variant: object) => TCompiledStyles;
+}
+
+export interface StyleEngine<
+    TCompiledStyles,
+    TModifiers extends Modifiers,
+    TComponents extends ExecutedThemeComponents<TModifiers>
+> {
+    compile: (
+        theme: ExecutedTheme<TModifiers, TComponents>
+    ) => StyleEngineResult<TCompiledStyles>;
+}
+
+export class RNStyleEngine
+    implements
+        StyleEngine<
+            StyleSheet.NamedStyles<unknown>,
+            Modifiers,
+            ExecutedThemeComponents<Modifiers>
+        >
+{
+    compile() {
+        return {
+            get: () => ({}),
+        };
+    }
+}
