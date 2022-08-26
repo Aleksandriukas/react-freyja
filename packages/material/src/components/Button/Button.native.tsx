0d7aa8b3a1546bfbd09d330c40c0668de5175d4f
Aleksandras Sukelovic <alex.shukel@gmail.com>
2022-08-26 12:27:29 +0300

    Initialized @react-freyja/base package

---
+++ packages/material/src/components/Button/Button.native.tsx
@@ -0,0 +1,31 @@
+import { ButtonBase } from "@react-freyja/base";
+import { useThemeContext } from "@react-freyja/theme-context";
+import React, { useMemo } from "react";
+import {
+    Button as NativeButton,
+    ButtonProps as NativeButtonProps,
+} from "react-native";
+import { ButtonProps } from "./ButtonProps";
+
+export const Button = ({
+    children,
+    color,
+    ...other
+}: ButtonProps & NativeButtonProps) => {
+    const getComponentStyles = useThemeContext();
+
+    const style = useMemo(
+        () =>
+            getComponentStyles("Button", { color } as unknown as Record<
+                string,
+                string
+            >),
+        [getComponentStyles, color]
+    );
+
+    return (
+        <ButtonBase Root={NativeButton} props={{ style, ...other }}>
+            {children}
+        </ButtonBase>
+    );
+};
