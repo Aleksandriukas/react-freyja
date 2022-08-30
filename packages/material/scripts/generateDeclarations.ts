1cf52046d4c6bb10707c3cb2108f1cf94a5f9c0f
Aleksandras Sukelovic <alex.shukel@gmail.com>
2022-08-30 12:50:55 +0300

    Generating d.ts file for @react-freyja/material package

--- packages/material/scripts/generateDeclarations.ts
+++ packages/material/scripts/generateDeclarations.ts
@@ -1,11 +1,36 @@
 #!/usr/bin/env zxpp
 
-import path from "node:path";
+import { accessSync, mkdirSync, writeFileSync } from "fs";
+import path from "path";
 import { generateDtsBundle } from "dts-bundle-generator";
 
-// TODO
+const safeWriteFileSync = (filePath: string, content: string) => {
+    const directory = path.dirname(filePath);
+    try {
+        accessSync(directory);
+    } catch {
+        mkdirSync(path.dirname(filePath), { recursive: true });
+    }
+
+    writeFileSync(filePath, content);
+};
+
+const outputFile = path.resolve(__dirname, "..", "dist", "index.d.ts");
+
 generateDtsBundle([
     {
-        filePath: path.resol,
+        filePath: path.resolve(__dirname, "..", "src", "index.web.ts"),
+        libraries: {
+            importedLibraries: [
+                "react",
+                "react-native",
+                "node",
+                "prop-types",
+                "scheduler",
+                "@react-freyja/theme",
+            ],
+        },
     },
-]);
+]).map((bundle) => safeWriteFileSync(outputFile, bundle));
+
+console.log(`${outputFile} has been generated.`);
