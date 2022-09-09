bc7b5a13c544fa6ee5dccf8bf2f00fbf5e5f4234
Aleksandras Sukelovic <alex.shukel@gmail.com>
2022-09-09 12:49:49 +0300

    Fixed test

--- packages/theme/src/utils/markTokens.ts
+++ packages/theme/src/utils/markTokens.ts
@@ -1,19 +1,10 @@
 import {
     ConvertModifiers,
     MarkedToken,
-    MarkedTokenModifier,
     Modifiers,
     Token,
 } from "../types/Theme";
 import { tokenUniqueSymbol, tokenModifierUniqueSymbol } from "./uniqueSymbols";
-
-const defineTokenIdentifier = (token: object, id: symbol) => {
-    Object.defineProperty(token, "_type", {
-        value: id,
-        enumerable: false,
-        writable: false,
-    });
-};
 
 export const markTokens = (
     tokens: Record<string, Token>
@@ -21,9 +12,7 @@ export const markTokens = (
     const result = {} as Record<string, MarkedToken>;
 
     for (const tokenName of Object.keys(tokens)) {
-        result[tokenName] = { ...tokens[tokenName] } as MarkedToken;
-
-        defineTokenIdentifier(result[tokenName], tokenUniqueSymbol);
+        result[tokenName] = { ...tokens[tokenName], _type: tokenUniqueSymbol };
     }
 
     return result;
@@ -37,9 +26,8 @@ export const markModifierTokens = (
     for (const tokenName of Object.keys(modifiers)) {
         result[tokenName] = {
             ...modifiers[tokenName],
-        } as MarkedTokenModifier<Modifiers>;
-
-        defineTokenIdentifier(result[tokenName], tokenModifierUniqueSymbol);
+            _type: tokenModifierUniqueSymbol,
+        };
     }
 
     return result;
